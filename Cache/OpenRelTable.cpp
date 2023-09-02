980f93d843bd2c8577b2abdcf7cc5a5cc915e1ec
Madhav Prabhu <dpmadhav@hotmail.com>
2023-09-02 13:32:47 +0530

    Stage 4 Exercises

--- Cache/OpenRelTable.cpp
+++ Cache/OpenRelTable.cpp
@@ -34,16 +34,26 @@ OpenRelTable::OpenRelTable( ) {
 	*( RelCacheTable::relCache[ RELCAT_RELID ] ) = relCatRecordCacheEntry;
 
 	/*
-	 * Insert Attribute catalog entry to cache[1]
+	 * Insert Attribute catalog entry to cache[0]
 	 */
 
 	relCatBlock.getRecord( relCatRecord.data( ), RELCAT_SLOTNUM_FOR_ATTRCAT );
 	RelCacheTable::recordToRelCatEntry( relCatRecord.data( ), &relCatRecordCacheEntry.relCatEntry );
-	relCatRecordCacheEntry.recId.block = ATTRCAT_BLOCK;
+	relCatRecordCacheEntry.recId.block = RELCAT_BLOCK;
 	relCatRecordCacheEntry.recId.slot  = RELCAT_SLOTNUM_FOR_ATTRCAT;
 
 	RelCacheTable::relCache[ ATTRCAT_RELID ]	  = new RelCacheEntry;
 	*( RelCacheTable::relCache[ ATTRCAT_RELID ] ) = relCatRecordCacheEntry;
+
+	/*
+	 * Insert Students catalog to cache[0] -- 4.exercise
+	 */
+	relCatBlock.getRecord( relCatRecord.data( ), 2 );
+	RelCacheTable::recordToRelCatEntry( relCatRecord.data( ), &relCatRecordCacheEntry.relCatEntry );
+	relCatRecordCacheEntry.recId.block = RELCAT_BLOCK;
+	relCatRecordCacheEntry.recId.slot  = 2;
+	RelCacheTable::relCache[ 2 ]	   = new RelCacheEntry;
+	*( RelCacheTable::relCache[ 2 ] )  = relCatRecordCacheEntry;
 
 	/************ Setting up Attribute cache entries ************/
 	// (we need to populate attribute cache with entries for the relation catalog
@@ -64,6 +74,7 @@ OpenRelTable::OpenRelTable( ) {
 	attrCatBlock.getHeader( &attrCatHeader );
 	std::list<AttrCacheEntry> relCatAttrList;
 	std::list<AttrCacheEntry> attrCatAttrList;
+	std::list<AttrCacheEntry> studCatAttrList;
 
 	for ( int i = 0; i < attrCatHeader.numEntries; i++ ) {
 		attrCatBlock.getRecord( attrCatRecord.data( ), i );
@@ -83,10 +94,17 @@ OpenRelTable::OpenRelTable( ) {
 			attrListEntry.recId.slot   = i;
 			attrCatAttrList.push_back( std::move( attrListEntry ) );
 		}
+		if ( std::strcmp( attrCatRecord[ ATTRCAT_REL_NAME_INDEX ].sVal, "students" ) == 0 ) {
+			attrListEntry.attrCatEntry = *attrData;
+			attrListEntry.recId.block  = ATTRCAT_BLOCK;
+			attrListEntry.recId.slot   = i;
+			studCatAttrList.push_back( std::move( attrListEntry ) );
+		}
 	}
 
 	AttrCacheTable::attrCache[ RELCAT_RELID ]  = std::move( relCatAttrList );
 	AttrCacheTable::attrCache[ ATTRCAT_RELID ] = std::move( attrCatAttrList );
+	AttrCacheTable::attrCache[ 2 ]			   = std::move( studCatAttrList );
 
 	/**** setting up Attribute Catalog relation in the Attribute Cache Table ****/
 
@@ -102,6 +120,9 @@ int OpenRelTable::getRelId( char relName[ ATTR_SIZE ] ) {
 
 	if ( std::strcmp( relName, "ATTRIBUTECAT" ) == 0 )
 		return ATTRCAT_RELID;
+
+	if ( std::strcmp( relName, "students" ) == 0 )
+		return 2;
 
 	return E_RELNOTOPEN;
 }
